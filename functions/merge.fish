5d38cb88df93e562d07ec624710f85a522b559a5
Dang <huynhminhdang@gmail.com>
2022-04-08 23:28:47 +0700

    Create merge.fish

---
+++ functions/merge.fish
@@ -0,0 +1,20 @@
+function merge --description "merge directories with rsync"
+  set --function dryrun "--dry-run"
+  if isatty stdin
+    read --local answer
+    if test "$anwser" = "y"
+      set --erase dryrun
+    end
+  end
+  if test (count $argv) -lt 2
+    echo no src/dest dirs. you moron
+    return
+  end
+  for arg in argv
+    if not string match --quiet '*/' $src
+      set arg "$arg/"
+    end
+    set --append --function dirs $arg
+  end
+  rsync --archive --verbose --progress $dryrun $dirs
+end
