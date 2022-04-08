13fe19351cd6b7accdf633b12e4161ce6507d3ad
Dang <huynhminhdang@gmail.com>
2022-04-08 23:57:29 +0700

    Update merge.fish

--- functions/merge.fish
+++ functions/merge.fish
@@ -1,20 +1,40 @@
-function merge --description "merge directories with rsync"
-  set --function dryrun "--dry-run"
-  if not isatty stdin
-    read --local answer
-    if test "$anwser" = "y"
-      set --erase dryrun
-    end
-  end
-  if test (count $argv) -lt 2
-    echo no src/dest dirs. you moron
-    return
-  end
-  for arg in argv
-    if not string match --quiet '*/' $src
-      set arg "$arg/"
-    end
-    set --append --function dirs $arg
-  end
-  rsync --archive --verbose --progress $dryrun $dirs
+function merge --description 'merge directories with rsync'
+   set --function dryrun
+   if not isatty stdin
+     read --local answer
+     if test "$answer" = y
+       set --erase dryrun
+     end
+   end
+   set --function rsync_argv
+   set --function rsync_dirs
+   for arg in $argv
+     if string match --quiet -- '-*' $arg
+       set --append rsync_argv $arg
+     else
+       if not string match --quiet '*/' -- $arg
+         set arg "$arg/"
+       end
+       set --append rsync_dirs $arg
+     end
+   end
+   if test (count $rsync_dirs) -lt 2
+     echo no src/dest dirs. you moron
+     return
+   end
+   set --append rsync_argv --archive --verbose --remove-source-files
+   set --query dryrun
+   if set --query dryrun
+     set --append rsync_argv --dry-run
+   else
+     set --append rsync_argv --progress
+   end
+   set --function rsync_dest $rsync_dirs[-1]
+   set --erase rsync_dirs[-1]
+   for src in $rsync_dirs
+     set_color green
+     echo rsync $rsync_argv $src $rsync_dest
+     set_color normal
+     rsync $rsync_argv $src $rsync_dest
+   end
 end
