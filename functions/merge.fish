cef18f2555cf154a983ccd62eac74a792d16b609
Dang <huynhminhdang@gmail.com>
2022-09-19 09:59:34 +0700

    use short flags

--- functions/merge.fish
+++ functions/merge.fish
@@ -1,40 +1,40 @@
-function merge --description 'merge directories with rsync'
-  set --function dryrun
+function merge -d 'merge directories with rsync'
+  set -f dryrun
   if not isatty stdin
-    read --local answer
+    read -l answer
     if test "$answer" = y
-      set --erase dryrun
+      set -e dryrun
     end
   end
-  set --function rsync_argv
-  set --function rsync_dirs
+  set -f rsync_argv
+  set -f rsync_dirs
   for arg in $argv
-    if string match --quiet -- '-*' $arg
-      set --append rsync_argv $arg
+    if string match -q -- '-*' $arg
+      set -a rsync_argv $arg
     else
-      if not string match --quiet '*/' -- $arg
+      if not string match -q '*/' -- $arg
         set arg "$arg/"
       end
-      set --append rsync_dirs $arg
+      set -a rsync_dirs $arg
     end
   end
   if test (count $rsync_dirs) -lt 2
     echo no src/dest dirs. you moron
     return
   end
-  set --append rsync_argv --archive --verbose --remove-source-files --ignore-existing --progress
-  set --query dryrun
-  set --function rsync_dest $rsync_dirs[-1]
-  set --erase rsync_dirs[-1]
+  set -a rsync_argv --archive --verbose --remove-source-files --ignore-existing --progress
+  set -q dryrun
+  set -f rsync_dest $rsync_dirs[-1]
+  set -e rsync_dirs[-1]
   for src in $rsync_dirs
     set_color green
     printf rsync
     for arg in $rsync_argv $src $rsync_dest
-      printf ' %s' (string escape -- \\$arg | string sub --start 3)
+      printf ' %s' (string escape -- \\$arg | string sub -s 3)
     end
     printf \n
     set_color normal
-    if set --query dryrun
+    if set -q dryrun
       rsync $rsync_argv --dry-run $src $rsync_dest
     else
       rsync $rsync_argv $src $rsync_dest
