f1c36425fada5b7f0024d32289a3c07e8fa03f1c
Dang <huynhminhdang@gmail.com>
2025-08-21 19:30:44 +0800

    redone

--- README.md
+++ README.md
@@ -10,15 +10,10 @@ fisher install dangh/merge.fish
 ### Usage
 
 ```sh
-# dryrun
 merge src_dir_1 src_dir_2 ... dest_dir
 
-# confirm merge
-yes | merge src_dir_1 src_dir_2 ... dest_dir
-
-# additional args can be appended to the command with --name=value format
-# exclude dot files for example
-merge src_dir_1 src_dir_2 ... dest_dir --exclude='.*'
+# exclude more files
+merge src_dir_1 src_dir_2 ... dest_dir -e '.*'
 ```
 
 ### Troubleshooting
--- functions/merge.fish
+++ functions/merge.fish
@@ -1,46 +1,69 @@
 function merge -d 'merge directories with rsync'
-    argparse f/force -- $argv
+    argparse 'e/exclude=' -- $argv
+    or return 1
 
-    set -f dryrun
-    if not isatty stdin
-        read -l answer
-        if test "$answer" = y
-            set -e dryrun
-        end
+    if test (count $argv) -lt 2
+        echo "Usage: merge [-e pattern ...] <src_dir> <src_dir> ... <dst_dir>"
+        return 1
     end
-    set -f rsync_argv
-    set -f rsync_dirs
-    for arg in $argv
-        if string match -q -- '-*' $arg
-            set -a rsync_argv $arg
-        else
-            if not string match -q '*/' -- $arg
-                set arg "$arg/"
+
+    set dst $argv[-1]
+    set srcs $argv[1..-2]
+
+    # build exclude list
+    set rsync_excludes "--exclude=.DS_Store"
+    for pattern in $_flag_exclude
+        set rsync_excludes $rsync_excludes "--exclude=$pattern"
+    end
+
+    for src in $srcs
+        echo $src
+
+        set -l new_files
+        set -l changed_files
+        set -l identical_files
+
+        # 1. dry-run with rsync to get changed files
+        rsync -rcvn --itemize-changes $rsync_excludes $src/ $dst/ | while read -d ' ' -l code file
+            switch $code
+                case ">f+++++++++"
+                    set -a new_files $src/$file
+                case ">f*"
+                    set -a changed_files $src/$file
             end
-            set -a rsync_dirs $arg
         end
-    end
-    if test (count $rsync_dirs) -lt 2
-        echo no src/dest dirs. you moron
-        return
-    end
-    set -a rsync_argv --archive --verbose --remove-source-files --progress
-    set -q _flag_force || set -a rsync_argv --ignore-existing
-    set -f rsync_dest $rsync_dirs[-1]
-    set -e rsync_dirs[-1]
-    for src in $rsync_dirs
-        set_color green
-        printf rsync
-        for arg in $rsync_argv $src $rsync_dest
-            printf ' %s' (string escape -- \\$arg | string sub -s 3)
+
+        # 2. find all files and exclude the changed files to get identical files
+        find $src -type f | sort | while read -l file
+            set base (string sub -s (math 2+(string length $src)) $file)
+            if contains $file $new_files
+                echo "  $base" (set_color green)"(new)"(set_color normal)
+            else if contains $file $changed_files
+                echo "  $base" (set_color red)"(changed)"(set_color normal)
+            else
+                echo "  $base"
+                set -a identical_files $file
+            end
         end
-        printf \n
-        set_color normal
-        if set -q dryrun
-            rsync $rsync_argv --dry-run $src $rsync_dest
-        else
-            rsync $rsync_argv $src $rsync_dest
-            find $src -type d -empty -delete
+
+        # 3. confirm
+        read -l -P "Proceed with move? (y/N) " answer
+        switch $answer
+            case y Y
+                # copy new/overwrite files
+                rsync -rcv --remove-source-files --progress $rsync_excludes $src/ $dst/
+
+                # remove identical files
+                for f in $identical_files
+                    if test -f $f
+                        rm $f
+                    end
+                end
+
+                # remove empty dirs
+                find $src -type d -empty -delete
+            case '*'
+                echo "Aborted."
         end
     end
 end
